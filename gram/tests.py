3499cd2c65c3034250c48edfa090c7c2a9a93f8f
Machuka Joy <machukajoy@gmail.com>
2022-06-05 21:25:35 +0300

    ts: tests profile mpdel instanciation and save methods

--- gram/tests.py
+++ gram/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import Profile
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+
+    def setUp(self):
+        self.Machuka = Profile(name = 'msmachuka', profile_pic = 'image.jpeg', bio='Coder and living life')
+        self.Machuka.save()
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Machuka, Profile))
+
+    def test_save_profile(self):
+        self.Machuka.save_profile()
+        name = Profile.objects.all()
+
+
