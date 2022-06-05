99275332df0266c6aa6171cf68ecdc29d9f43f87
Machuka Joy <machukajoy@gmail.com>
2022-06-06 00:21:30 +0300

    file: adds some media to use

--- gram/urls.py
+++ gram/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 from . import views
 urlpatterns=[
   path('',views.home,name = 'home'),
+  path('accounts/register/', views.register, name='register'),
+  
 
 
 ]
--- instagram/settings.py
+++ instagram/settings.py
@@ -144,6 +144,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 ACCOUNT_ACTIVATION_DAYS = 7
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 cloudinary.config(
     cloud_name = os.environ.get("CLOUD_NAME"), 
