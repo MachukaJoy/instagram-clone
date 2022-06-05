7caa2b4921246974550fd951eb4d253e3609e00e
Machuka Joy <machukajoy@gmail.com>
2022-06-06 00:00:42 +0300

    ft: adds form to register method that renders reg form

--- gram/views.py
+++ gram/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.http  import HttpResponse
 from .models import Profile
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from .forms import SignUpForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 # Create your views here.
@@ -11,4 +14,15 @@ def home(request):
 @login_required(login_url='/accounts/login/')
 
 def register(request):
-    return render(request,'registration/registration_form.html')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/registration_form.html', {'form': form})
