da87ed2f370b1ab36596d04fe765e3a528ad42d6
Machuka Joy <machukajoy@gmail.com>
2022-06-07 12:09:37 +0300

    ft: add view methods to render image to homepage, registration and create new post

--- gram/views.py
+++ gram/views.py
@@ -1,15 +1,54 @@
 from django.shortcuts import render, redirect
 from django.http  import HttpResponse
-from .models import Profile
+from .models import Profile, Post
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
-from .forms import SignUpForm
+from .forms import SignUpForm, NewPostForm
 from django.contrib.auth.models import User
+from .forms import UserUpdateForm, ProfileUpdateForm, Post
 
 # Create your views here.
-# Create your views here.
 def home(request):
-    return render(request,'home.html')
+    posts = Post.all_posts()
+    json_posts = []
+    for post in posts:
+        pic = Profile.objects.filter(user=post.user.id).first()
+        pic = pic.profile_pic.url
+        obj = dict(
+            image=post.image.url,
+            author=post.user.username,
+            avatar=pic,
+            name=post.title,
+            caption=post.caption
+
+        )
+        json_posts.append(obj)
+    return render(request, 'home.html', {"posts": json_posts})
+
+
+def profile(request):
+    if request.method == 'POST':
+
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+
+            return redirect('profile')
+
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+        context = {
+            'user_form': user_form,
+            'profile_form': profile_form
+
+        }
+
+    return render(request, 'profile.html', context)
 
 @login_required(login_url='/accounts/login/')
 
@@ -26,3 +65,16 @@ def register(request):
     else:
         form = SignUpForm()
     return render(request, 'registration/registration_form.html', {'form': form})
+
+def new_post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.user = current_user  
+            image.save()  
+        return redirect('home')
+    else:
+        form = NewPostForm()
+    return render(request, 'new_post.html', {"form": form})
